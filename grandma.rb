55c4f3ed3d51b89c5b315e678dba4047940d15da
Simon Balean <simonbalean@gmail.com>
2018-11-13 23:12:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,20 +5,13 @@
 # unless you shout it (type in all capitals).
 
 
-def speak_to_grandma(speech)
-  if speech == speech.upcase
-    if speech == "I LOVE YOU GRANDMA"
-      elsif puts "NO, NOT SINCE 1938!"
-  elsif puts "HUH?! SPEAK UP, SONNY"
-end
 
 def speak_to_grandma(speech)
   if speech == "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif speech == speech.upcase
-  return "NO, NOT SINCE 1938!"
-  elsif 
-  elsif puts "HUH?! SPEAK UP, SONNY"
+    return "NO, NOT SINCE 1938!"
+  else return "HUH?! SPEAK UP, SONNY!"
 end
 
 # If you shout, she can hear you (or at least she thinks so) 
