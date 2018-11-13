eac1a1525e35264d48d0075cf33817a296ec558d
Simon Balean <simonbalean@gmail.com>
2018-11-13 23:15:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@
 
 
 def speak_to_grandma(speech)
-  if speech == "I LOVE YOU GRANDMA"
+  if speech == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif speech == speech.upcase
     return "NO, NOT SINCE 1938!"
