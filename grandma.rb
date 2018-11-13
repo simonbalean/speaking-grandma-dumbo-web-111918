4f97630bdd407c3889695ec86dbb0fee84e58470
Simon Balean <simonbalean@gmail.com>
2018-11-13 23:04:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,8 @@
 # unless you shout it (type in all capitals).
 
 def speak_to_grandma(speech)
-  if speech = speech.upcase
-    if speech = "I LOVE YOU GRANDMA"
+  if speech == speech.upcase
+    if speech == "I LOVE YOU GRANDMA"
       puts "I LOVE YOU TOO PUMPKIN!"
     else puts "NO, NOT SINCE 1938!"
   else puts "HUH?! SPEAK UP, SONNY"
